5734d6eafc38f037d23b4de142255063d4def54e
LJW0907 <jwlee0109@gmail.com>
2023-05-18 08:22:33 +0900

    read_line 함수 추가

--- data_structure_string/ds_string.c
+++ data_structure_string/ds_string.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 int strlen_user(char *str) {
-	char word;
 	int len = 0;
 
 	for (int i = 0; str[i] != '\0'; i++) {
@@ -12,14 +11,30 @@ int strlen_user(char *str) {
 	return len;
 }
 
+int read_line(char* str, int limit) { //단점을 모두 보완한 문자열 입력 함수
+	int ch, i = 0;
+
+	while ((ch = getchar()) != '\n')
+		if (i < limit)
+			str[i++] = ch;
+
+	str[i] = '\0';
+
+	return i; //문자열 길이 반환
+ }
+
 int main() {
-	char str[100];
+	char str[10];
 
-	printf("& ");
-	
-	scanf("%[^\n]s", str);
-	//gets(str); 안전하지 않으므로 사용 지양
-	//fgets(str, 100, stdin); //stdin(표준 입출력) 방식으로 100글자를 입력해서 str에 저장
+	while (1) {
+		printf("& ");
 
-	printf("%s:%d", str, strlen_user(str));
+		//scanf_s("%[^\n]*c", str, 10);
+		//gets(str); 안전하지 않으므로 사용 지양
+		fgets(str, 10, stdin); //stdin(표준 입출력) 방식으로 100글자를 입력해서 str에 저장
+		str[strlen(str) - 1] = '\0';
+		//fgets 단점 : 줄바꿈 문자(\n)까지 포함해서 str로 입력하기 때문에 오차 발생
+
+		printf("%s:%d\n", str, strlen_user(str));
+	}
 }
