ec09d5a11578c033bb314e195c05632e366adf39
LJW0907 <jwlee0109@gmail.com>
2023-05-18 06:56:08 +0900

    공백 포함 문자열 입력 scanf("[^\n}s", str) 구현

--- data_structure_string/ds_string.c
+++ data_structure_string/ds_string.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+int strlen_user(char *str) {
+	char word;
+	int len = 0;
+
+	for (int i = 0; str[i] != '\0'; i++) {
+		len++;
+	}
+
+	return len;
+}
 
 int main() {
+	char str[100];
 
+	printf("& ");
+	
+	scanf("%[^\n]s", str);
+
+	printf("%s:%d", str, strlen_user(str));
 }
