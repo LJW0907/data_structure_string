e27a9b7744e2948651e960ec95014ea42b6ce687
LJW0907 <jwlee0109@gmail.com>
2023-05-18 07:04:08 +0900

    gets()와 fgets() 방법 주석으로 추가

--- data_structure_string/ds_string.c
+++ data_structure_string/ds_string.c
@@ -18,6 +18,8 @@ int main() {
 	printf("& ");
 	
 	scanf("%[^\n]s", str);
+	//gets(str); 안전하지 않으므로 사용 지양
+	//fgets(str, 100, stdin); //stdin(표준 입출력) 방식으로 100글자를 입력해서 str에 저장
 
 	printf("%s:%d", str, strlen_user(str));
 }
